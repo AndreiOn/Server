a08536d04fb987e2b30dc79beec8b68c8ef74450
AndreiOn <43577964+AndreiOn@users.noreply.github.com>
2018-09-25 16:26:54 +0200

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,15 @@
+"""
+	Python 3.7 - Simple TCP/UDP Game Server
+"""
+
+print('\n\nBonjour et bienvenue ! ')
+print('Tapez "start" pour lancer le serveur ')
+
+do_start = input('§>')
+
+if do_start == 'start':
+	print('Attendez un peu... le serveur n\'a pas encore été totalement codé !')
+else:
+	print('Qu\'est-ce que vous dites !? Je ne comprends pas !')
+  
+print('\n\n À bientôt !')
